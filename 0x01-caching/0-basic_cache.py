2d3d2d95e55a1f419a93f0a6bc6a1bdc388e3708
adil mae <itsadilhadid@gmail.com>
2024-02-01 00:07:23 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Basic dictionary"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
